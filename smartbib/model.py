c394d5940f9edd43766c83921de88c308a549a2e
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2021-10-11 23:14:42 +0200

    ENH: Replace the hash ID by integer

--- smartbib/model.py
+++ smartbib/model.py
@@ -1,9 +1,9 @@
 from sqlalchemy import (
     Table, String, Text, Integer, Column, ForeignKey, MetaData
 )
-from sqlalchemy.dialects.mysql import insert
+from sqlalchemy.schema import ForeignKeyConstraint
+from sqlalchemy.dialects.mysql import insert, INTEGER, BIGINT
 from sqlalchemy.sql import select, update
-from types import SimpleNamespace
 
 
 class PaperDatabase:
@@ -13,14 +13,15 @@ class PaperDatabase:
         self.pdf_url = self._gen_table_pdf_url()
         self.fos = self._gen_table_fos()
         self.author = self._gen_table_author()
-        self.authorship = self._gen_table_authorship()
+        #  self.authorship = self._gen_table_authorship()
         self.citation = self._gen_table_citation()
 
     def _gen_table_paper(self):
         return Table(
             'papers', self.metadata_obj,
-            Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('id_paper', String(40), unique=True),
+            Column('id_1', BIGINT(unsigned=True), primary_key=True),
+            Column('id_2', BIGINT(unsigned=True), primary_key=True),
+            Column('id_3', INTEGER(unsigned=True), primary_key=True),
             Column('title', String(512)),
             Column('paper_abstract', Text),
             Column('year', Integer),
@@ -29,44 +30,76 @@ class PaperDatabase:
             Column('journal_name', String(256)),
             Column('journal_volume', String(256)),
             Column('journal_pages', String(256)),
-            Column('doi', String(256)),
-            Column('doi_url', String(256))
         )
 
     def _gen_table_pdf_url(self):
         return Table(
             'pdf_urls', self.metadata_obj,
-            Column('id_paper', Integer, ForeignKey('papers.id')),
-            Column('content', Text)
+            Column('id_paper_1', BIGINT(unsigned=True)),
+            Column('id_paper_2', BIGINT(unsigned=True)),
+            Column('id_paper_3', INTEGER(unsigned=True)),
+            Column('content', Text),
+            ForeignKeyConstraint(
+                ['id_paper_1', 'id_paper_2', 'id_paper_3'],
+                ['papers.id_1', 'papers.id_2', 'papers.id_3']
+            )
         )
 
     def _gen_table_fos(self):
         return Table(
             'fields_of_study', self.metadata_obj,
-            Column('id_paper', Integer, ForeignKey('papers.id')),
-            Column('content', String(40))
+            Column('id_paper_1', BIGINT(unsigned=True)),
+            Column('id_paper_2', BIGINT(unsigned=True)),
+            Column('id_paper_3', INTEGER(unsigned=True)),
+            Column('content', String(40)),
+            ForeignKeyConstraint(
+                ['id_paper_1', 'id_paper_2', 'id_paper_3'],
+                ['papers.id_1', 'papers.id_2', 'papers.id_3']
+            )
+
         )
 
     def _gen_table_author(self):
         return Table(
             'authors', self.metadata_obj,
-            Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('id_author', Integer, unique=True),
-            Column('name', String(256))
+            Column('id_author', Integer),
+            Column('name', String(256)),
+            Column('id_paper_1', BIGINT(unsigned=True)),
+            Column('id_paper_2', BIGINT(unsigned=True)),
+            Column('id_paper_3', INTEGER(unsigned=True)),
+            ForeignKeyConstraint(
+                ['id_paper_1', 'id_paper_2', 'id_paper_3'],
+                ['papers.id_1', 'papers.id_2', 'papers.id_3']
+            )
         )
 
-    def _gen_table_authorship(self):
-        return Table(
-            'authorship', self.metadata_obj,
-            Column('id_author', Integer, ForeignKey('authors.id')),
-            Column('id_paper', Integer, ForeignKey('papers.id')),
-        )
+    #  def _gen_table_authorship(self):
+    #      return Table(
+    #          'authorship', self.metadata_obj,
+    #          Column('id_author', Integer, ForeignKey('authors.id')),
+    #          Column('id_paper_1', BIGINT(unsigned=True)),
+    #          Column('id_paper_2', BIGINT(unsigned=True)),
+    #          Column('id_paper_3', INTEGER(unsigned=True)),
+    #          ForeignKeyConstraint(
+    #              ['id_paper_1', 'id_paper_2', 'id_paper_3'],
+    #              ['papers.id_1', 'papers.id_2', 'papers.id_3']
+    #          )
+    #
+    #      )
 
     def _gen_table_citation(self):
         return Table(
             'citations', self.metadata_obj,
-            Column('id_cited', Integer, ForeignKey('papers.id')),
-            Column('id_citer', Integer, ForeignKey('papers.id')),
+            Column('id_cited_1', BIGINT(unsigned=True)),
+            Column('id_cited_2', BIGINT(unsigned=True)),
+            Column('id_cited_3', INTEGER(unsigned=True)),
+            Column('id_citer_1', BIGINT(unsigned=True)),
+            Column('id_citer_2', BIGINT(unsigned=True)),
+            Column('id_citer_3', INTEGER(unsigned=True)),
+            ForeignKeyConstraint(
+                ['id_cited_1', 'id_cited_2', 'id_cited_3'],
+                ['papers.id_1', 'papers.id_2', 'papers.id_3']
+            )
         )
 
 
